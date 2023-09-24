67010b0e06a48156e4ae9ce21f9d05cb77438406
Éric <nascimento3ric@gmail.com>
2023-09-23 22:12:32 -0300

    Atividade lista de convidados, conexão e comunição com API Efetivada

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -1,7 +1,8 @@
 package com.example.demo.controller;
 
-import java.sql.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,37 +11,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-
-    private String nome = "";
     
+    ArrayList<String> confirmados = new ArrayList<>();
+    Map<String, String> convidados = new HashMap<>(){{
 
-    ArrayList<String> convidados = new ArrayList<String>();
+        put("0bf33b824502fd061e388d1a66566d98", "João Silva Xavier Pereira");
+        put("018cce2808f152e871147666ceda13f1", "Maria Santos Rosa Machado");
+        put("C1684a804be7534b9e4363359ccf229f", "Pedro Oliveira da Silva Mentes");
+        put("6123ff798247b5ce99236ade746bb9ed", "Ana Pereira de Oliveira Mello");
+        put("05e3e0e55e64b131ef4e2cc1169a8c50", "Luis Costa de Souza Santos");
+    }};
 
-
-  
-    // João Silva Xavier Pereira 0bf33b824502fd061e388d1a66566d98
-    // Maria Santos Rosa Machado 018cce2808f152e871147666ceda13f1
-    // Pedro Oliveira da Silva Mentes C1684a804be7534b9e4363359ccf229f
-    // Ana Pereira de Oliveira Mello 6123ff798247b5ce99236ade746bb9ed
-    // Luís Costa de Souza Santos 05e3e0e55e64b131ef4e2cc1169a8c50
-
-    @GetMapping(value = "presenca/{token}")
-    public void confirmToken(@PathVariable ArrayList<String> convidados) {
-        this.convidados = convidados;
+    @GetMapping(value = "adicionar/{token}")
+    public void adicionarConvidado(@PathVariable String token){
+        this.confirmados.add(convidados.get(token));
     }
 
-    @GetMapping(value = "/add/{nome}")
-    public void setNome(@PathVariable String nome) {
-        this.nome = nome;
+    @RequestMapping(value = "/deletar")
+    public void removerConvidados(){
+        this.confirmados.clear();
     }
 
     @RequestMapping(value = "/list")
-    public String showNome() {
-        return "O nome digitado foi " + this.nome;
-    }
+    public String Listar(){
+        if(confirmados.isEmpty()){
+            return "Lista vazia";
+        }
 
-    public String getNome() {
-        return this.nome;
+        String listaConfirmados = String.join(", ", confirmados);
+        return "Confirmados para a festa: " + listaConfirmados;
     }
 
 }
